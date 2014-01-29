6bea5a23a4067a5eef23ea8727b96b6dd289df6d
Silas Davis <silas@swiftkey.net>
2014-01-29 15:20:00 +0000

    changed project groupid

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject overtone/at-at "1.2.0"
-  :description "Ahead-of-time function scheduler"
+(defproject silasdavis/at-at "1.2.0"
+  :description "Ahead-of-time function scheduler - with serviced pull requests"
+  :url "https://github.com/silasdavis/at-at"
   :dependencies [[org.clojure/clojure "1.3.0"]])
