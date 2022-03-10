02a3a8b4b48e5d01e5926d1fb07175484fa7aac4
xlfe <felixb@gmail.com>
2022-03-10 21:40:02 +1100

    add license details to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject net.xlfe/at-at "1.3.1"
   :description "Ahead-of-time function scheduler"
   :url "https://github.com/xlfe/at-at"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]])
